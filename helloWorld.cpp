ef8446a39479dd8f530f6818f1e25b6cd27b141b
Qiongjie Tian <tqjustc@gmail.com>
2018-06-10 11:05:39 -0700

    add small

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -6,11 +6,13 @@ int main()
 	int i;
 	int[] a = {1,2,3,4};
 	int sum = 0;
+    int small = 100;
 	for(i = 0; i < 4; i++)
 	{
 		sum += a[i];
+        if(small > a[i])
+            small = a[i];
 	}
 	cout << "sum = " << sum <<endl;
-    cout << "hello world." <<endl;
 	return 0;
 }
