b0ea1c0909688917b323ab1dd56405f7d67c8670
Yashu <tqjustc@hotmail.com>
2018-06-10 11:07:01 -0700

    add averge

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -6,10 +6,12 @@ int main()
 	int i;
 	int[] a = {1,2,3,4};
 	int sum = 0;
+	int averge = 0;
 	for(i = 0; i < 4; i++)
 	{
 		sum += a[i];
 	}
+	averge = sum / 4;
 	cout << "sum = " << sum <<endl;
     cout << "hello world." <<endl;
 	return 0;
